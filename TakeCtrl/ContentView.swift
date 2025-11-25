8a8d814b26b82ee6840891c1919350d0d69104c6
zacheri04 <chalegoy5@gmail.com>
2025-11-25 01:33:29 -0600

    Initial Commit

---
+++ TakeCtrl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TakeCtrl
+//
+//  Created by Zack on 11/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TakeCtrl/TakeCtrlApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeCtrlApp.swift
+//  TakeCtrl
+//
+//  Created by Zack on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TakeCtrlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
